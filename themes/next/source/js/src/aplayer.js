c7b5fd3a10bec216d7e423e05ac171804fc3c125
Alex.Don.Scofield <lizhiqin46783937@live.com>
2020-03-29 20:23:15 +0800

    add aplayer

---
+++ themes/next/source/js/src/aplayer.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  autoplay: false,
+  audio: [
+    {
+      name: "Falling Into A Dream",
+      artist: 'Brian Tyler',
+      url: 'https://blog-music-host.oss-cn-shanghai.aliyuncs.com/music/Brian%20Tyler%20-%20Falling%20Into%20A%20Dream.mp3',
+    }
+  ]
+});
